2d7ba5413eed7f091ca40a44eef48d9e2995a366
rodion <rodionaaafgh@gmail.com>
2019-01-25 18:53:55 +0300

    feature: deploy script

---
+++ frontend/management/deploy.sh
@@ -0,0 +1,5 @@
+npm install
+npm run build
+aws s3 cp build s3://$BUCKET --recursive
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
